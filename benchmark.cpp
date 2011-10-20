d3d9d2240f545496c3abafdbd11ec1f569dc0e4c
Erik Reed <ereed.uw@gmail.com>
2011-10-19 23:39:31 -0700

    first testcase for fft. i was unable to compile due to library issues so i haven't tested this...

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,4 +1,18 @@
-int main(int args, char* argv[]) {
+#include <iostream>
+using namespace std;
 
+int main(int args, char* argv[]) {
+	hestonTests();
+}
+
+void hestonTests() {
+	double tol = 1e-6;
+
+	double fft_cuda1 = HestonCallFFT(2,.04,.1,.5,.04,.01,.3,1,.8,4096);
+	double fft_matlab1 = 0.203756595588233;
+	
+	cout << "-- fft_test1 --" << endl;
+	cout << "cuda: " << fft_cuda1 << endl;
+	cout << "matlab: " << fft_matlab1 << endl;
 }
 
