b923985ff877fd28016ff0f237c097d8511133b4
William Chan <williamchan@cmu.edu>
2011-10-21 12:02:23 -0700

    BlackScholes call price calculator

--- BlackScholes.cpp
+++ BlackScholes.cpp
@@ -1,4 +1,6 @@
 #include "BlackScholes.hpp"
+#include <math.h>
+#include <gsl/gsl_cdf.h>
 
 
 double implied(
@@ -10,4 +12,15 @@ double implied(
   return 0;
 }
 
+double call(
+  double dS0,
+  double dK,
+  double dR,
+  double dT,
+  double dSigma) {
+  double dF = dS0 * exp(dR * dT);
+  double d1 = log(dF / dK) / (dSigma * sqrt(dT)) + dSigma * sqrt(dT) / 2;
+  double d2 = log(dF / dK) / (dSigma * sqrt(dT)) - dSigma * sqrt(dT) / 2;
 
+  return exp(-dR * dT) * (dF * gsl_cdf_ugaussian_P(d1) - dK * gsl_cdf_ugaussian_P(d2));
+}
--- BlackScholes.hpp
+++ BlackScholes.hpp
@@ -8,5 +8,12 @@ double implied(
   double dT,
   double dCallPrice);
 
+double call(
+  double dS0,
+  double dK,
+  double dR,
+  double dT,
+  double dSigma);
+
 #endif
 
