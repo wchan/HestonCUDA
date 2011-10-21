3d74897cc41f7439b8f7eff300fe03e9c1d2b668
William Chan <williamchan@cmu.edu>
2011-10-21 02:54:41 -0700

    BlackScholes template.. we will need a hill climbing algorithm

---
+++ BlackScholes.cpp
@@ -0,0 +1,13 @@
+#include "BlackScholes.hpp"
+
+
+double implied(
+  double dS0,
+  double dK,
+  double dR,
+  double dT,
+  double dCallPrice) {
+  return 0;
+}
+
+
---
+++ BlackScholes.hpp
@@ -0,0 +1,12 @@
+#ifndef __BLACKSCHOLES_HPP__
+#define __BLACKSCHOLES_HPP__
+
+double implied(
+  double dS0,
+  double dK,
+  double dR,
+  double dT,
+  double dCallPrice);
+
+#endif
+
