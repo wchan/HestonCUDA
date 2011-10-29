7ec2e486ef7f7517bba6253558a8301592a0c0e7
Erik Reed <ereed.uw@gmail.com>
2011-10-28 19:34:48 -0700

    indentation on quad. returning nan.

--- HestonCallQuadCPU.cpp
+++ HestonCallQuadCPU.cpp
@@ -83,21 +83,21 @@ inline void legendre(
     }
     else if (ind == 2) {
         //*quad_1d_point = {-1.0/sqrt(3.0), 1.0/sqrt(3.0)};
-q[0]= -1.0/sqrt(3.0);
-q[1]= 1.0/sqrt(3.0);
+        q[0]= -1.0/sqrt(3.0);
+        q[1]= 1.0/sqrt(3.0);
         //*quad_1d_weight = {1, 1};
-w[0] = 1;
-w[1] = 1;
+        w[0] = 1;
+        w[1] = 1;
     }
     else if (ind == 3) {
         //*quad_1d_point = {-sqrt(3.0/5.0), 0, sqrt(3.0/5.0)};
-q[0] = -sqrt(3.0/5.0); 
-q[1] = 0; 
-q[2] = sqrt(3.0/5.0);
+        q[0] = -sqrt(3.0/5.0); 
+        q[1] = 0; 
+        q[2] = sqrt(3.0/5.0);
         //*quad_1d_weight = {5.0/9.0, 8.0/9.0, 5.0/9.0};
-w[0]= 5.0/9.0; 
-w[1]= 8.0/9.0; 
-w[2]= 5.0/9.0;
+        w[0]= 5.0/9.0; 
+        w[1]= 8.0/9.0; 
+        w[2]= 5.0/9.0;
     }
     else if (ind == 4) {
         double a,b,w1,w2;
@@ -106,15 +106,15 @@ w[2]= 5.0/9.0;
         w1 = (18.0-sqrt(30.0))/36.0;
         w2 = (18.0+sqrt(30.0))/36.0;
         //*quad_1d_point = {-a, -b, b, a};
-q[0] = -a; 
-q[1] = -b; 
-q[2] = b; 
-q[3] = a;
+        q[0] = -a; 
+        q[1] = -b; 
+        q[2] = b; 
+        q[3] = a;
         //*quad_1d_weight = {w1, w2, w2, w1};
-w[0] = w1;
-w[1] = w2;
-w[2] = w2;
-w[3] = w1;
+        w[0] = w1;
+        w[1] = w2;
+        w[2] = w2;
+        w[3] = w1;
     }
     else if (ind == 5) {
         double a,b,w1,w2;
@@ -123,17 +123,17 @@ w[3] = w1;
         w1 = (322.0-13.0*sqrt(70.0))/900.0;
         w2 = (322.0+13.0*sqrt(70.0))/900.0;
         //*quad_1d_point = {-a, -b, 0, b, a};
-q[0] = -a; 
-q[1] = -b; 
-q[2] = 0; 
-q[3] = b; 
-q[4] = a;
+        q[0] = -a; 
+        q[1] = -b; 
+        q[2] = 0; 
+        q[3] = b; 
+        q[4] = a;
         //*quad_1d_weight = {w1, w2, 128.0/225.0, w2, w1};
-w[0] = w1; 
-w[1] = w2;
-w[2] = 128.0/225.0; 
-w[3] = w2; 
-w[4] = w1;
+        w[0] = w1; 
+        w[1] = w2;
+        w[2] = 128.0/225.0; 
+        w[3] = w2; 
+        w[4] = w1;
     }
     else if (ind == 16) {
         double a1,a2,a3,a4,a5,a6,a7,a8;
@@ -159,39 +159,39 @@ w[4] = w1;
         //*quad_1d_weight = {w8, w7, w6, w5, w4, w3, w2, 
         //    w1, w1, w2, w3, w4, w5, w6, w7, w8};
 
-q[0] = -a8; 
-q[1] = -a7; 
-q[2] = -a6; 
-q[3] = -a5; 
-q[4] = -a4; 
-q[5] = -a3; 
-q[6] = -a2; 
-q[7] = -a1; 
-q[8] = a1;
-q[9] = a2; 
-q[10] = a3; 
-q[11] = a4; 
-q[12] = a5; 
-q[13] = a6; 
-q[14] = a7; 
-q[15] = a8;
+        q[0] = -a8; 
+        q[1] = -a7; 
+        q[2] = -a6; 
+        q[3] = -a5; 
+        q[4] = -a4; 
+        q[5] = -a3; 
+        q[6] = -a2; 
+        q[7] = -a1; 
+        q[8] = a1;
+        q[9] = a2; 
+        q[10] = a3; 
+        q[11] = a4; 
+        q[12] = a5; 
+        q[13] = a6; 
+        q[14] = a7; 
+        q[15] = a8;
 
-w[0] = w8; 
-w[1] = w7; 
-w[2] = w6; 
-w[3] = w5; 
-w[4] = w4; 
-w[5] = w3; 
-w[6] = w2; 
-w[7] = w1; 
-w[8] = w1; 
-w[9] = w2; 
-w[10] = w3; 
-w[11] = w4; 
-w[12] = w5; 
-w[13] = w6; 
-w[14] = w7; 
-w[15] = w8;
+        w[0] = w8; 
+        w[1] = w7; 
+        w[2] = w6; 
+        w[3] = w5; 
+        w[4] = w4; 
+        w[5] = w3; 
+        w[6] = w2; 
+        w[7] = w1; 
+        w[8] = w1; 
+        w[9] = w2; 
+        w[10] = w3; 
+        w[11] = w4; 
+        w[12] = w5; 
+        w[13] = w6; 
+        w[14] = w7; 
+        w[15] = w8;
 
 
     }
@@ -256,7 +256,7 @@ inline double quad_(
     //Q=h*y*w;
     //double* Q = new double[N];
     double Q = 0;
-    
+
     for (int i=0; i<N; i++) {
         y[i] = hestonPIntegrand(x[i], kappa, theta, 
                 sigma, rho, v0, r, T, s0, K, type);
@@ -295,10 +295,7 @@ inline double HestonP(
     //TODO: fix pi; return type of quad_
     return 0.5 + 1/pi*
         quad_(0,100,N, kappa, theta,sigma,rho,v0,r,T,s0,K,type);
-
-
 }
-
 
 double HestonCallQuadCPU(
         double kappa,
@@ -318,3 +315,4 @@ double HestonCallQuadCPU(
         K*std::exp(-r*T)*HestonP(kappa,theta,sigma,rho,v0,r,T,s0,K,2,N);
 
 }
+
