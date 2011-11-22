7dbe08cc7b69324ba5a7dc15487a65467ecb58d5
Erik Reed <ereed.uw@gmail.com>
2011-11-22 03:08:34 -0800

    attempt to use transform_iterators

--- HestonCallQuadGPU.cu
+++ HestonCallQuadGPU.cu
@@ -23,6 +23,16 @@
 #include <thrust/iterator/constant_iterator.h>
 #include <thrust/iterator/permutation_iterator.h>
 
+struct ModPfunc {
+    const int p;
+    ModPfunc(int p) :p(p) {}
+    
+    __host__ __device__
+    int operator()(const int& i) const {
+        return i%p;
+    }
+};
+
 struct HestonCallQuadGPU_functor1 {
 	const HestonCUDAPrecision dKappa;
 	const HestonCUDAPrecision dTheta;
@@ -63,16 +73,6 @@ struct HestonCallQuadGPU_functor1 {
 	}
 };
 
-//    for (int i=0; i<N; i++) {
-//		** func1 **
-//		x = a+h*((2*(i/p)+1)+quad[i%p]);
-//		** func2 **
-//		w = w_[i%p];
-//		y = HestonP_GPUIntegrand(x, kappa, theta,
-//				sigma, rho, v0, r, T, s0, K, type);
-//		Q += y*w;
-//    }
-
 	struct HestonCallQuadGPU_functor2 {
 		const HestonCUDAPrecision dKappa;
 		const HestonCUDAPrecision dTheta;
@@ -110,17 +110,8 @@ struct HestonCallQuadGPU_functor1 {
 		__host__ __device__
 		HestonCUDAPrecision operator()(const HestonCUDAPrecision& x, const HestonCUDAPrecision& w) {
 			HestonCUDAPrecision y,Q;
-			y = HestonP_GPUIntegrand(x, dKappa, dTheta,
-					dSigma, dRho, dV0, dR, dT, dS0, dStrike, type);
-
-//			std::cout<<x<<" " << dKappa << " " << dTheta
-//					<< " " << dSigma << " " << dRho <<" "
-//				<< dV0 << " " << dR << " " << dT << " "
-//					<< dS0 << " " << dStrike << " " << type <<
-//					y << std::endl;
-
+			y = HestonP_GPUIntegrand(x, dKappa, dTheta, dSigma, dRho, dV0, dR, dT, dS0, dStrike, type);
 			Q = y*w;
-			//std::cout << "x: " << x << "\ty: " << y << std::endl;
 			return Q;
 		}
 
@@ -161,15 +152,9 @@ struct HestonCallQuadGPU_functor1 {
 	            - 2.0*log((1.0-g*exp(d*T))/(1.0-g)));
 
 	    D = (b-rho*sigma*phi*zI + d)/(sigma*sigma)*((1.0-exp(d*T))/ (1.0-g*exp(d*T)));
-	    //std::cout << D.x << " " << D.y << std::endl;
-
 	    f = exp(C + D*v0 + zI*phi*x);
-
-	    //std::cout << (C + D*v0 + zI*phi*x).x << " " << (C + D*v0 + zI*phi*x).y << std::endl;
-
 	    return f;
 	}
-
 
 	__host__ __device__ inline
 	HestonCUDAPrecision HestonP_GPUIntegrand(
@@ -207,19 +192,15 @@ __host__ inline void legendre_GPU(
         w[0] = 2;
     }
     else if (ind == 2) {
-        //*quad_GPU1d_point = {-1.0/sqrt(3.0), 1.0/sqrt(3.0)};
         q[0]= -1.0/sqrt(3.0);
         q[1]= 1.0/sqrt(3.0);
-        //*quad_GPU1d_weight = {1, 1};
         w[0] = 1;
         w[1] = 1;
     }
     else if (ind == 3) {
-        //*quad_GPU1d_point = {-sqrt(3.0/5.0), 0, sqrt(3.0/5.0)};
         q[0] = -sqrt(3.0/5.0);
         q[1] = 0;
         q[2] = sqrt(3.0/5.0);
-        //*quad_GPU1d_weight = {5.0/9.0, 8.0/9.0, 5.0/9.0};
         w[0]= 5.0/9.0;
         w[1]= 8.0/9.0;
         w[2]= 5.0/9.0;
@@ -230,12 +211,10 @@ __host__ inline void legendre_GPU(
         b = sqrt((3.0-2.0*sqrt(6.0/5.0)/7.0));
         w1 = (18.0-sqrt(30.0))/36.0;
         w2 = (18.0+sqrt(30.0))/36.0;
-        //*quad_GPU1d_point = {-a, -b, b, a};
         q[0] = -a;
         q[1] = -b;
         q[2] = b;
         q[3] = a;
-        //*quad_GPU1d_weight = {w1, w2, w2, w1};
         w[0] = w1;
         w[1] = w2;
         w[2] = w2;
@@ -247,13 +226,11 @@ __host__ inline void legendre_GPU(
         b = 1.0/3.0*sqrt(5.0-2.0*sqrt(10.0/7.0));
         w1 = (322.0-13.0*sqrt(70.0))/900.0;
         w2 = (322.0+13.0*sqrt(70.0))/900.0;
-        //*quad_GPU1d_point = {-a, -b, 0, b, a};
         q[0] = -a;
         q[1] = -b;
         q[2] = 0;
         q[3] = b;
         q[4] = a;
-        //*quad_GPU1d_weight = {w1, w2, 128.0/225.0, w2, w1};
         w[0] = w1;
         w[1] = w2;
         w[2] = 128.0/225.0;
@@ -279,10 +256,6 @@ __host__ inline void legendre_GPU(
         w6 = 0.0951585116824927848099251;
         w7 = 0.0622535239386478928628438;
         w8 = 0.0271524594117540948517806;
-        //*quad_GPU1d_point = {-a8, -a7, -a6, -a5, -a4, -a3,
-        //    -a2, -a1, a1, a2, a3, a4, a5, a6, a7, a8};
-        //*quad_GPU1d_weight = {w8, w7, w6, w5, w4, w3, w2,
-        //    w1, w1, w2, w3, w4, w5, w6, w7, w8};
 
         q[0] = -a8;
         q[1] = -a7;
@@ -317,8 +290,6 @@ __host__ inline void legendre_GPU(
         w[13] = w6;
         w[14] = w7;
         w[15] = w8;
-
-
     }
     else
         throw "likely an error in legendre_GPU... ind != to anything." ;
@@ -339,15 +310,10 @@ __host__ inline HestonCUDAPrecision quad_GPU(
         HestonCUDAPrecision K, 
         int type) {
 
-    HestonCUDAPrecision h; // var c isn't ever used
-    //c = (a + b)/2.0;
-    h = (b - a)/2.0;
-
-    h=(b-a)/(N-1);
+    HestonCUDAPrecision h = (b-a)/(N-1);
 
     // tested with p=2 only
     const int p=2; // fixed in quad_GPU
-
 
     HestonCUDAPrecision* w_ = new HestonCUDAPrecision[p];
     HestonCUDAPrecision* quad = new HestonCUDAPrecision[p];
@@ -360,18 +326,17 @@ __host__ inline HestonCUDAPrecision quad_GPU(
     // TODO: using int not long...
     thrust::counting_iterator<int> counter(0);
     thrust::counting_iterator<int> counter_last = counter+N;
+    /*
     thrust::constant_iterator<int> p_it(p);
-
     thrust::device_vector<int> indicies(N);
     thrust::transform(counter, counter_last, p_it, indicies.begin(), thrust::modulus<int>());
+    
 
     thrust::device_vector<HestonCUDAPrecision> dev_full_quad(N);
     thrust::device_vector<HestonCUDAPrecision> dev_full_w(N);
 
-
-
     // get w_ and quad vecs set up
-    //TODO: use fancy iterators here and for indicies
+    
     thrust::copy(
     		thrust::make_permutation_iterator(dev_quad.begin(),indicies.begin()),
     		thrust::make_permutation_iterator(dev_quad.begin(),indicies.end()),
@@ -382,47 +347,46 @@ __host__ inline HestonCUDAPrecision quad_GPU(
         		thrust::make_permutation_iterator(dev_w.begin(),indicies.end()),
         		dev_full_w.begin()
     );
-    //debugging
-//    thrust::copy(dev_full_w.begin(),dev_full_w.end(),std::ostream_iterator<double>(std::cout," "));
-//    thrust::copy(dev_w.begin(),dev_w.end(),std::ostream_iterator<double>(std::cout,"\n\n"));
-
+    */
     thrust::device_vector<HestonCUDAPrecision> dev_Q(N);
     HestonCallQuadGPU_functor1 func1(kappa,theta,sigma,rho,v0,r,T,s0,K,p,a,h,type); // calculates x
     HestonCallQuadGPU_functor2 func2(kappa,theta,sigma,rho,v0,r,T,s0,K,p,a,h,type); // calculates Q
+    ModPfunc modP(p);
+    //TODO: combine these w/ zip iterator
 
-    thrust::transform(counter, counter_last,
-    		dev_full_quad.begin(),
-    		dev_Q.begin(), func1);
+    thrust::transform(
+            counter,
+            counter_last,
+            thrust::make_permutation_iterator(
+                dev_quad.begin(),
+                thrust::make_transform_iterator(
+                    counter,
+                    modP()
+                )
+            ), 
+            dev_Q.begin(), 
+            func1
+    );
 
-
-    thrust::transform(dev_Q.begin(), dev_Q.end(),
-        		dev_full_w.begin(),
-        		dev_Q.begin(), func2);
-
-    //thrust::copy(dev_Q.begin(),dev_Q.end(),std::ostream_iterator<double>(std::cout,"\n"));
-
+    thrust::transform(
+            dev_Q.begin(),
+            dev_Q.end(),
+            thrust::make_permutation_iterator(
+                dev_w.begin(),
+                thrust::make_transform_iterator(
+                    counter,
+                    modP()
+                )
+            ),
+            dev_Q.begin(), 
+            func2
+    );
 
     HestonCUDAPrecision Q = thrust::reduce(dev_Q.begin(), dev_Q.end());
-//    for (int i=0; i<N; i++) {
-//		x = a+h*((2*(i/p)+1)+quad[i%p]);
-//		w = w_[i%p];
-//		y = HestonP_GPUIntegrand(x, kappa, theta,
-//				sigma, rho, v0, r, T, s0, K, type);
-//		Q += y*w;
-//    }
     Q *= h;
-
-//    free(dev_full_w);
-//	free(dev_full_quad);
-//	free(dev_quad);
-//	free(dev_w);
-//	free(indicies);
-//	free(dev_Q);
+    
     delete[] quad;
     delete[] w_;
-//    delete[] x;
-//    delete[] w;
-    //delete[] y;
     return Q;
 }
 
